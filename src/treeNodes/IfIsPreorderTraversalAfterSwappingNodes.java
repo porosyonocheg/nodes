02c7e06437d2e5eaad1886d254956183e943b883
Sergey Shershavin <mourinho89@rambler.ru>
2021-04-29 08:34:09 +0300

    IfIsPreorderTraversalAfterSwappingNodes.class was modified

--- src/treeNodes/IfIsPreorderTraversalAfterSwappingNodes.java
+++ src/treeNodes/IfIsPreorderTraversalAfterSwappingNodes.java
@@ -1,6 +1,8 @@
 package treeNodes;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /** Массив preorder представляет собой искомый preorder обход дерева. Если переданное дерево можно привести к данному
  * массиву путём перемены потомков любых его узлов, то вернуть список узлов, потомки которых изменили положение. Если
@@ -9,32 +11,27 @@ import java.util.*;
 
 public class IfIsPreorderTraversalAfterSwappingNodes extends Command {
     private final int[] preorder;
+    private final List<Integer> result;
+    private int index;
     public IfIsPreorderTraversalAfterSwappingNodes(TreeNode root, int[] preorder) {
         super(root);
         this.preorder = preorder;
+        result = new ArrayList<>();
+        index = 0;
+    }
+
+    private boolean flipMatchVoyage(TreeNode node, int[] preorder) {
+        if (node == null) return true;
+        if (node.val != preorder[index++]) return false;
+        if (node.left != null && node.left.val != preorder[index]) {
+            result.add(node.val);
+            return flipMatchVoyage(node.right, preorder) && flipMatchVoyage(node.left, preorder);
+        }
+        return flipMatchVoyage(node.left, preorder) && flipMatchVoyage(node.right, preorder);
     }
 
     @Override
     public Object execute() {
-        List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        int i = 0;
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            TreeNode current = stack.pop();
-            if (current.val != preorder[i++]) return Collections.singletonList(-1);
-            if (current.right != null && current.right.val == preorder[i]) {
-                if (current.left != null) {
-                    result.add(current.val);
-                    stack.push(current.left);
-                }
-                if (current.right != null) stack.push(current.right);
-            }
-            else {
-                if (current.right != null) stack.push(current.right);
-                if (current.left != null) stack.push(current.left);
-            }
-        }
-        return result;
+        return flipMatchVoyage(root, preorder) ? result : Collections.singletonList(-1);
     }
 }
